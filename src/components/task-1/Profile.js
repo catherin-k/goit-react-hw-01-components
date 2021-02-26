9c31a4148d6618798145c560b3c115acc60c740b
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2021-02-26 16:47:49 +0200

    change task-1 render

--- src/components/task-1/Profile.js
+++ src/components/task-1/Profile.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import Stats from './Stats';
+import PropTypes from 'prop-types';
+import s from './Profile.module.css';
 
 const Profile = ({ name, tag, location, avatar, stats }) => {
+  const { followers, views, likes } = stats;
   return (
-    <div>
-      <div>
-        <img src={avatar} alt="User-avatar" />
-        <p>{name}</p>
-        <p>{tag}</p>
-        <p>{location}</p>
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img src={avatar} alt={name} className={s.avatar} />
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
-      <ul>
-        <Stats stats={stats} />
+
+      <ul className={s.stats}>
+        <li className={s.statsItem}>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{followers}</span>
+        </li>
+        <li className={s.statsItem}>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{views}</span>
+        </li>
+        <li className={s.statsItem}>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{likes}</span>
+        </li>
       </ul>
     </div>
   );
