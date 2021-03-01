f02019befc06097983b23cade10135b911720c66
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2021-03-01 22:52:13 +0200

    add styles for task-4

--- src/components/task-4/TransactionHistory.js
+++ src/components/task-4/TransactionHistory.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import s from './TransactionHistory.module.css';
 
 const TransactionHistory = ({ items }) => (
-  <table>
+  <table className={s.transactionHistory}>
     <thead>
       <tr>
         <th>Type</th>
